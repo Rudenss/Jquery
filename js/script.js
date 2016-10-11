485a6b3290e2bcbf8c5c19bddfe2ab7dc31893a1
aaa <kail27@naver.com>
2016-10-11 14:41:07 +0900

    test

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function()
+{
+  $('input#content').focus(function()
+    {
+      console.log('focusing to content');
+    });
+    $('input#content').blur(function()
+      {
+        console.log('blur to content');
+      });
+    $('select').change(function()
+    {
+      console.log('select value changed');
+    });
+    $('input#content').keydown(function()
+    {
+      console.log('keydown');
+    });
+    $('input#content').keyup(function()
+    {
+      console.log('keyup');
+    });
+    $('input#act').click(function()
+    {
+      console.log('act butten click');
+    });
+    $('input#act').hover(function()
+    {
+      console.log('act butten mouse over');
+    });
+    $('input#toggle').click(function()
+    {
+      $('p.target').toggle(1000,function()
+      {
+        console.log('toggle it');
+      });
+    });
+});
